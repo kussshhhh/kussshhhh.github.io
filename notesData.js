6567e19507d00c96f413047bc70e2f569b6d7520
kussshhhh <kushsep21@gmail.com>
2025-02-27 00:49:02 +0530

    might work

--- notesData.js
+++ notesData.js
@@ -1,48 +1,15 @@
-// Auto-generated notes graph data
-// Generated on: imported directly
-const notesData = {
+const graphData = {
   nodes: [
-  {
-    "id": "go",
-    "group": 10
-  },
-  {
-    "id": "servers",
-    "group": 8
-  },
-  {
-    "id": "scripts",
-    "group": 8
-  },
-  {
-    "id": "projects",
-    "group": 13
-  },
-  {
-    "id": "something",
-    "group": 10
-  }
-],
+    { "id": "go", "group": 10 },
+    { "id": "servers", "group": 8 },
+    { "id": "scripts", "group": 8 },
+    { "id": "projects", "group": 13 },
+    { "id": "something", "group": 10 }
+  ],
   links: [
-  {
-    "source": "go",
-    "target": "servers",
-    "value": 1
-  },
-  {
-    "source": "go",
-    "target": "scripts",
-    "value": 1
-  },
-  {
-    "source": "go",
-    "target": "something",
-    "value": 1
-  },
-  {
-    "source": "go",
-    "target": "projects",
-    "value": 1
-  }
-]
+    { "source": "go", "target": "servers", "value": 1 },
+    { "source": "go", "target": "scripts", "value": 1 },
+    { "source": "go", "target": "something", "value": 1 },
+    { "source": "go", "target": "projects", "value": 1 }
+  ]
 };
